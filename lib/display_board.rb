db4f9cf9c8d3f862e62a8a228f066656985057f3
Tracy Reuther <tracyreuther@gmail.com>
2019-05-04 11:12:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+  phrase = "A Tic Tac Toe Board"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  print display_board
+  print phrase
 end
