a7c41858e0db98a451dd5a8b087f677d3cdba0e2
Tracy Reuther <tracyreuther@gmail.com>
2019-05-04 11:36:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 def display_board
   board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  put "-----------"
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  put "-----------"
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board
